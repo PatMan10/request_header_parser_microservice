15b7ae868a965677a45303ab8261e3fbf569d41b
PatMan10 <PatMan10@protonmail.com>
2022-01-22 09:11:46 +0200

    small tweak to models.ts

--- src/app/main/code/models.ts
+++ src/app/main/code/models.ts
@@ -3,6 +3,6 @@ export class RequestMeta {
     readonly ipaddress: string = "127.0.0.1",
     readonly language: string = "en-GB,en;q=0.5",
     readonly software: string =
-      "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Chromium/97.0",
+      `Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Chromium/97.0`,
   ) {}
 }
